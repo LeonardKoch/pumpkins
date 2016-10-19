7d9b505a5277c978a815cb8ae65f8b86b35d9b3b
Leonard Koch <Leonard@LeonardKoch.com>
2016-10-19 23:55:51 +0200

    Adjust location of pumpkin images.

--- pumpkins/src/App.js
+++ pumpkins/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import pumpkin1 from './images/pumpkin_lit_1.jpg';
 import './App.css';
 
 class App extends Component {
@@ -9,6 +10,7 @@ class App extends Component {
                 <div className="App-header">
                     <img src={logo} className="App-logo" alt="logo" />
                     <h2>Welcome to React</h2>
+                    <img src={pumpkin1} />
                 </div>
                 <p className="App-intro">
                     To get started, edit <code>src/App.js</code> and save to reload.
