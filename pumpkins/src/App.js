0c092c0ff6cdfa27d908d24697fdeacf0250d114
Leonard Koch <l.koch@mytaxi.com>
2016-10-20 18:15:04 +0200

    Final state.

--- pumpkins/src/App.js
+++ pumpkins/src/App.js
@@ -1,21 +1,47 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import pumpkinDark from './images/pumpkin_dark.jpg';
+import pumpkin0 from './images/pumpkin_lit_0.jpg';
 import pumpkin1 from './images/pumpkin_lit_1.jpg';
+import pumpkin2 from './images/pumpkin_lit_2.jpg';
+import pumpkin3 from './images/pumpkin_lit_3.jpg';
+import pumpkin4 from './images/pumpkin_lit_4.jpg';
+import pumpkin5 from './images/pumpkin_lit_5.jpg';
 import './App.css';
 
+const pumpkins = [
+    pumpkin0,
+    pumpkin1,
+    pumpkin2,
+    pumpkin3,
+    pumpkin4,
+    pumpkin5,
+];
+
 class App extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            pumpkins: []
+        };
+    }
+
+    addPumpkin() {
+        this.setState({pumpkins: [...this.state.pumpkins, Math.round(Math.random()*5)]})
+    }
+
     render() {
         return (
-            <div className="App">
-                <div className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <h2>Welcome to React</h2>
-                    <img src={pumpkin1} />
+            <div className="App" style={{backgroundColor: '#412301', height: '100vh'}}>
+                <div>
+                    {this.state.pumpkins.map((pumpkinImageNumber) => <Pumpkin pumpkinImageNumber={pumpkinImageNumber} />)}
                 </div>
-                <p className="App-intro">
-                    To get started, edit <code>src/App.js</code> and save to reload.
-                </p>
-                <Pumpkin />
+                <div>
+                    <button onClick={() => this.addPumpkin()} style={{lineHeight: '30px', color: 'white', backgroundColor: 'orange'}}>
+                        Add a Pumpkin!
+                    </button>
+                </div>
             </div>
         );
     }
@@ -23,10 +49,45 @@ class App extends Component {
 
 
 class Pumpkin extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            lit: false
+        };
+    }
+
+    light() {
+        this.setState({
+            lit: true
+        });
+    }
+
+    dim() {
+        this.setState({
+            lit: false
+        });
+    }
+
     render() {
-        return <div>Pumpkin</div>
+        return <img src={this.state.lit ? pumpkins[this.props.pumpkinImageNumber] : pumpkinDark}
+                    onClick={() => {
+                        if(this.state.lit)
+                        {
+                            this.dim()
+                        }
+                        else
+                        {
+                            this.light()
+                        }
+                    }}
+                    style={pumpkinStyle}
+                />
     }
 }
 
+const pumpkinStyle = {
+    width: '200px'
+};
 
 export default App;
